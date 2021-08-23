477a0548a2b57ab2cbfa6494299ce0fdd528d8a2
vivek pabari <vivek.v.pabari@gmail.com>
2021-08-23 16:24:10 +0530

    Update app.py

--- todolist/app.py
+++ todolist/app.py
@@ -1,1 +1,55 @@
+#library
+from flask import Flask,render_template,request,flash,url_for,redirect
+from flask_sqlalchemy import SQLAlchemy
+import logging
+
+
+app = Flask(__name__)
+app.config['SECRECT_KEY'] = 'XYZABC'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+db = SQLAlchemy(app)
+
+
+class create_user(db.Model):
+    user_id = db.Column(db.Integer(),primary_key = True,autoincrement = True)
+    user_name = db.Column(db.String(40),nullable = False)
+    user_email = db.Column(db.String(40),nullable = False)
+    user_password = db.Column(db.String(64),nullable = False)
+
+class create_list(db.Model):
+    list_id = db.Column(db.Integer(),primary_key = True,autoincrement = True)
+    list_text = db.Column(db.Text(),nullable = False)
+    list_user_id = db.Column(db.Integer())#,db.ForeignKey('create_user.user_id'))
+
+@app.route('/')
+def index():
+    items = ['assignment','homework']
+    return render_template('todolist.html',flag = True,items = create_list.query.all() )
+
+@app.route('/add',methods = ['GET','POST'])
+def add():
+    if request.method == 'POST':
+        if not request.form['task']:
+            flash("Please Enter Work Name")
+        else:
+            add_item = create_list(list_text=request.form['task'],list_user_id=1)
+            db.session.add(add_item)
+            db.session.commit()
+        return redirect('/')
+    else:
+        logging.error("INVALID REQUEST")
+        
+
+
+@app.route('/remove',methods = ['GET','POST'])
+def remove():
+    create_list.query.filter_by(list_id=request.args.get('id')).delete()
+    db.session.commit()
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
+    
 
